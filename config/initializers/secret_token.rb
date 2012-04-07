667d4915d99f4d4b6cd77b8cb022af9609f1fd77
Rico Marcelo <rico.c.marcelo@gmail.com>
2012-04-06 21:25:15 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XmlTest::Application.config.secret_token = 'a9bdc3fbea5562976647e99a3f0f347e586919afa2f7f2a6a1881afe41a783aebb65e6c18b2f81a654e415aeff5c5d176637d1af91e69bbdda3223f1240afcea'
